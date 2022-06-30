7254fa240a9aaaa3345dff4e9afa470fa14270cc
Artur Dani <darthur83@hotmail.com>
2022-06-30 22:41:13 +0000

    add PokemonCard component

--- src/pages/Pokemons.js
+++ src/pages/Pokemons.js
@@ -1,10 +1,17 @@
+import PokemonCard from "../components/PokemonCard";
 import SearchForm from "../components/SearchForm";
+
+const pokemons = [1, 2, 3, 4];
 
 const Pokemons = () => {
   return (
     <>
       <SearchForm />
-      <div>Pokemons</div>
+      <div className="grid w-full grid-cols-1 mx-auto mt-8 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-5 gap-y-6">
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon} />
+        ))}
+      </div>
     </>
   );
 };
